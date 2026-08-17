c247d9c6d16a37c5a08d23f2db4cd95de1304ffd
kareem-ayman12 <kimob1425@gmail.com>
2026-08-17 15:20:10 +0300

    server and controllers is done

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db';
+const port = process.env.PORT || 3000;
+const app: Application = express();
+app.use(express.json());
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+})
