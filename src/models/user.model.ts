e1d8679ea256673f9c114979881796170c3ebcd4
kamel-ahmed0 <kamosahmed2018@gmail.com>
2026-08-17 17:13:57 +0300

    adding routes and creating user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true
+        },
+        lastname:{
+            type:String,
+            required:true
+        }
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true
+    }
+})
+export const User = mongoose.model('User', UserSchema);
