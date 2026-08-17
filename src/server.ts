9967c534e55454573de117be5ba0f79f17b860cc
kareem-ayman12 <kimob1425@gmail.com>
2026-08-17 15:34:20 +0300

    connected to mongodb and running the server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 const port = process.env.PORT || 3000;
 const app: Application = express();
+dotenv.config();
 app.use(express.json());
 connectDB().then(() => {
     app.listen(port, () => {
